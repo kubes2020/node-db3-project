938a91d68d9ddbccad002dee4a2a9aa5e0bd77df
Brian Kubes <briankubes@gmail.com>
2020-11-04 13:44:06 -0800

    findSteps(id) is woring

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,39 @@
 // scheme-model
+const db = require("../data/db-config");
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+function find() {
+    return db('schemes')
+}
+function findById(id) {
+    return db('schemes').where({id}).first()
+}
+function findSteps(id) {
+    return db('schemes as sc').join('steps as st', 'sc.id', 'st.scheme_id').select('sc.id', 'sc.scheme_name', 'st.step_number', 'st.instructions').orderBy('st.step_number').where({'sc.id': id})
+}
+
+// select
+//     sc.id,
+//     sc.scheme_name,
+//     st.step_number,
+//     st.instructions 
+// from Schemes as sc
+// join Steps as st
+//     on sc.id= st.scheme_id
+//     order by sc.scheme_name, st.step_number;
+
+function add() {
+    return console.log('wired')
+}
+function update() {
+    return console.log('wired')
+}
+function remove() {
+    return console.log('wired')
+}
